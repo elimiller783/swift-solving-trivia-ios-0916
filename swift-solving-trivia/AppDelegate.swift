89d3ece3dac9dfe86b52550a69ef5990a949713d
elimiller783 <emiller783@gmail.com>
2016-09-16 00:11:51 -0400

    one function

--- swift-solving-trivia/AppDelegate.swift
+++ swift-solving-trivia/AppDelegate.swift
@@ -11,36 +11,107 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    //
+    //    func matchName(state: String, capital: String) -> Bool {
+    //        for letter in state.characters{
+    //            for letters in capital.characters {
+    //                if letter == letters {
+    //                    return false
+    //                }
+    //            }
+    //        }
+    //        return true
+    //    }
+    //
+    //
+    //
+    //
+    //    func solveTrivia(states: [String : String]) -> String {
+    //        var answer = ""
+    //        for (state, capital) in states {
+    //            if matchName(state.lowercaseString, capital: capital.lowercaseString) {
+    //                answer = state
+    //            }
+    //        }
+    //        return answer
+    //    }
+    //}
     
-    func matchName(state: String, capital: String) -> Bool {
-        for letter in state.characters{
-            for letters in capital.characters {
-                if letter == letters {
-                    return false
+//    var states = [
+//    
+//    "Alabama":"Montgomery",
+//    "Alaska":"Juneau",
+//    "Arizona":"Phoenix",
+//    "Arkansas":"Little Rock",
+//    "California":"Sacramento",
+//    "Colorado":"Denver",
+//    "Connecticut":"Hartford",
+//    "Delaware":"Dover",
+//    "Florida":"Tallahassee",
+//    "Georgia":"Atlanta",
+//    "Hawaii":"Honolulu",
+//    "Idaho":"Boise",
+//    "Illinois":"Springfield",
+//    "Indiana":"Indianapolis",
+//    "Iowa":"Des Moines",
+//    "Kansas":"Topeka",
+//    "Kentucky":"Frankfort",
+//    "Lousiana":"Baton Rouge",
+//    "Maine":"Augusta",
+//    "Maryland":"Annapolis",
+//    "Massassachusetts":"Boston",
+//    "Michigan":"Lansing",
+//    "Minnesota":"Saint Paul",
+//    "Mississippi":"Jackson",
+//    "Missouri":"Jefferson City",
+//    "Montana":"Helena",
+//    "Nebraska":"Lincoln",
+//    "Nevada":"Carson City",
+//    "New Hampshire":"Concord",
+//    "New Jersey":"Trenton",
+//    "New Mexico":"Santa Fe",
+//    "New York":"Albany",
+//    "North Carolina":"Raleigh",
+//    "North Dakota":"Bismarck",
+//    "Ohio":"Columbus",
+//    "Oklahoma":"Oklahoma City",
+//    "Oregon":"Salem",
+//    "Pennsylvania":"Harrisburg",
+//    "Rhode Island":"Providence",
+//    "South Carolina":"Columbia",
+//    "South Dakota":"Pierre",
+//    "Tennessee":"Nashville",
+//    "Texas":"Austin",
+//    "Utah":"Salt Lake City",
+//    "Vermont":"Montpelier",
+//    "Virgina":"Richmond",
+//    "Washington":"Olympia",
+//    "West Virginia":"Charleston",
+//    "Wisconsin":"Madison",
+//    "Wyoming":"Cheyenne"]
+//    
+    
+    func solveTrivia(var dict: [String : String]) -> String {
+        var answer = ""
+        for (state, capital) in dict {
+            let stateChar = Array(state.lowercaseString.characters)
+            let capitalChar = Array(capital.lowercaseString.characters)
+            
+            for letter in stateChar {
+                if capitalChar.contains(letter) {
+                    dict.removeValueForKey(state)
                 }
             }
+            
         }
-        return true
-    }
-    
-    
-    
-    
-    func solveTrivia(states: [String : String]) -> String {
-        var answer = ""
-        for (state, capital) in states {
-            if matchName(state.lowercaseString, capital: capital.lowercaseString) {
-                answer = state
-            }
+        for (state, _) in dict {
+            answer = state
+            
         }
-        return answer
-    }
+    return answer
+    
+    
 }
-
-
-
-
-
-
+}
 
 
